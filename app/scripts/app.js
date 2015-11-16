df28f8340aad90dbaed318a583523f1474e7a151
Maksim Łabovič <maksim.labovicz@gmail.com>
2015-11-16 14:54:25 +0300

    big commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,20 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import './bootstrap-modals.js';
+import './bootstrap-tooltip.js';
+import './bootstrap-dropdown.js';
+import './jquery.details.min.js';
 
 $(() => {
 	svg4everybody();
+
+
+	if (!Modernizr.details) {
+    $('details').details();
+  }
+  $('[data-toggle="tooltip"]').tooltip()
 });
+
+// ($('.js-login').on('click',function (){
+
+// }))();
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,13 +4,18 @@ import gutil from 'gulp-util';
 import gulpif from 'gulp-if';
 import rupture from 'rupture';
 import stylus from 'gulp-stylus';
-import autoprefixer from 'autoprefixer-stylus';
+import autoprefixer from 'autoprefixer';
 import cmq from 'gulp-group-css-media-queries';
 import minifyCss from 'gulp-minify-css';
 import csscomb from 'gulp-csscomb';
+import postcss from 'gulp-postcss';
+import cssgrace from 'cssgrace';
 import rename from 'gulp-rename';
+import rgbaFallback from 'postcss-color-rgba-fallback';
+import fontWeight from 'postcss-font-weights';
 import errorHandler from '../utils/errorHandler';
 import paths from '../paths';
+import { browsers } from '../../package.json';
 
 gulp.task('styles', () => (
 	gulp.src('*.styl', {
@@ -21,14 +26,27 @@ gulp.task('styles', () => (
 		.pipe(stylus({
 			errors: true,
 			use: [
-				rupture(),
-				autoprefixer()
+				rupture()
 			],
 			sourcemap: gutil.env.debug ? {
 				comment: false,
 				inline: true
 			} : false
 		}))
+		.pipe(postcss([
+        rgbaFallback(),
+        autoprefixer(
+            'Android >= ' + browsers.android,
+            'Chrome >= ' + browsers.chrome,
+            'Firefox >= ' + browsers.firefox,
+            'Explorer >= ' + browsers.ie,
+            'iOS >= ' + browsers.ios,
+            'Opera >= ' + browsers.opera,
+            'Safari >= ' + browsers.safari
+          ),
+        fontWeight()
+        ]
+      ))
 		.pipe(gulpif(!gutil.env.debug, cmq()))
 		.pipe(gulpif(!gutil.env.debug, minifyCss()))
 		.pipe(gulpif(gutil.env.csscomb, csscomb()))
