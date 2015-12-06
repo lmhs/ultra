cc8dcb07d12f315e01592e71755168ed7cd57410
Maksim Łabovič <maksim.labovicz@gmail.com>
2015-12-07 00:39:57 +0300

    showcase live

--- app/scripts/blocks/carousel.js
+++ app/scripts/blocks/carousel.js
@@ -19,4 +19,12 @@ import $ from 'jquery';
     nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"><i class="icon-arrow-right-fff-13x24 slick-next__icon"></i></button>',
     dots: true
   });
+	$('.js-live-carousel').slick({
+    prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"><i class="icon-arrow-left-fff-13x24 slick-prev__icon"></i></button>',
+    nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"><i class="icon-arrow-right-fff-13x24 slick-next__icon"></i></button>',
+    dots: false,
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 5
+  });
 }));
