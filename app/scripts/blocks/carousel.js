6c9053d1bb6a902613db3034882e2fb434513df3
Maksim Łabovič <maksim.labovicz@gmail.com>
2015-12-15 18:19:06 +0300

    product card adaptive

--- app/scripts/blocks/carousel.js
+++ app/scripts/blocks/carousel.js
@@ -17,7 +17,15 @@ import $ from 'jquery';
 	$('.js-carousel').slick({
 		prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"><i class="icon-arrow-left-fff-13x24 slick-prev__icon"></i></button>',
 		nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"><i class="icon-arrow-right-fff-13x24 slick-next__icon"></i></button>',
-		dots: true
+		dots: true,
+		responsive: [
+		{
+			breakpoint: 720,
+			settings: {
+				infinite: true,
+				arrows: false
+			}
+		}]
 	});
 	$('.js-live-carousel').slick({
 		prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"><i class="icon-arrow-left-fff-13x24 slick-prev__icon"></i></button>',
@@ -36,11 +44,12 @@ import $ from 'jquery';
 		slidesToScroll: 5,
 		responsive: [
 		{
-			breakpoint: 720,
+			breakpoint: 990,
 			settings: {
 				slidesToShow: 4,
 				slidesToScroll: 4,
 				infinite: true,
+				arrows: false,
 				dots: false
 			}
 		}]
