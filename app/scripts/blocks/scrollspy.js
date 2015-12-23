d5e1e343b01cf3dc49a58c4d5a5629082c2978f4
Maksim Łabovič <maksim.labovicz@gmail.com>
2015-12-23 12:11:09 +0300

    tabs scrollspy

---
+++ app/scripts/blocks/scrollspy.js
@@ -0,0 +1,20 @@
+import '../third-party/bootstrap-scrollspy.js'
+import $ from 'jquery';
+
+
+(function (factory) {
+		if (typeof define === 'function' && define.amd) {
+				// AMD. Register as an anonymous module.
+				define(['jquery'], factory);
+		} else if (typeof module === 'object' && module.exports) {
+				// Node/CommonJS
+				module.exports = factory(require('jquery'));
+		} else {
+				// Browser globals
+				factory(jQuery);
+		}
+}(function ($) {
+	$('body').scrollspy({
+												target: '#js-product-info-navigation'
+												 });
+}));
