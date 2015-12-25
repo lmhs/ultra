516474c848b6afe1d5d624c973fd1de23b5c5925
Maksim Łabovič <maksim.labovicz@gmail.com>
2015-12-25 04:07:30 +0300

    product-lp-adaptive

--- app/scripts/blocks/carousel.js
+++ app/scripts/blocks/carousel.js
@@ -88,6 +88,34 @@ import $ from 'jquery';
 			settings: 'unslick'
 		}]
 	});
+	$('.js-other-carousel').slick({
+		prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"><i class="icon-arrow-left-fff-13x24 slick-prev__icon"></i></button>',
+		nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"><i class="icon-arrow-right-fff-13x24 slick-next__icon"></i></button>',
+		dots: false,
+		slidesToShow: 5,
+		slidesToScroll: 5,
+		responsive: [
+		{
+			breakpoint: 990,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				arrows: false,
+				dots: false
+			}
+		},{
+			breakpoint: 720,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				arrows: false,
+				dots: false
+			}
+		},{
+			breakpoint: 480,
+			settings: 'unslick'
+		}]
+	});
 	$('.js-announce-carousel').slick({
 		mobileFirst: true,
 		centerMode: true,
